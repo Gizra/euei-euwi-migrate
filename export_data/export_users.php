d7a86f867dfaa7ffedc75135606d030b4519f9ee
YURI <yuri@gizra.com>
2014-12-08 17:21:28 +0200

    Add script to export user to new table.

---
+++ export_data/export_users.php
@@ -0,0 +1,24 @@
+<?php
+  $total = db_result(db_query("SELECT COUNT(uid) FROM {users} u ORDER BY u.uid"));
+  $range = 50;
+  $count = 0;
+  drush_print ($total);
+
+  while($count < $total){
+    $result = db_query("SELECT uid FROM {users} u ORDER BY u.uid LIMIT %d OFFSET %d", $range, $count);
+
+    while ($row = db_fetch_array($result)) {
+      $user = user_load($row);
+      $insert = db_query("INSERT INTO _gizra_users(uid, `name`, password, mail)
+        VALUES('%d','%s','%s', '%s')", $user->uid, $user->name, $user->pass, $user->mail);
+      $count++;
+    }
+
+    $params = array(
+      '@count' => $count,
+      '@total' => $total,
+      '@id' => $user->uid,
+    );
+    drush_print(dt('(@count / @total) Processed user ID @id.', $params));
+  }
+?>
