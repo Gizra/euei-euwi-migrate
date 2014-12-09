95ea0a99536f8d059c14e03ea2bd54b51ddd19aa
YURI <yuri@gizra.com>
2014-12-09 17:29:13 +0200

    Remove print.Fix code.

--- export_data/export_users.php
+++ export_data/export_users.php
@@ -2,7 +2,6 @@
   $total = db_result(db_query("SELECT COUNT(uid) FROM {users} u WHERE status = 1 ORDER BY u.uid"));
   $range = 50;
   $count = 0;
-  drush_print ($total);
 
   while($count < $total){
     $result = db_query("SELECT uid FROM {users} u WHERE status = 1 ORDER BY u.uid LIMIT %d OFFSET %d", $range, $count);
@@ -10,8 +9,7 @@
     while ($user = user_load(db_fetch_array($result))) {
       $insert = db_query(
         "INSERT INTO _gizra_users(uid, name, password, mail) VALUES('%d', '%s', '%s', '%s')",
-        $user->uid, $user->name, $user->pass, $user->mail
-      );
+        $user->uid, $user->name, $user->pass, $user->mail);
       $count++;
       $params = array(
         '@count' => $count,
@@ -21,4 +19,3 @@
       drush_print(dt('(@count / @total) Processed user ID @id.', $params));
     }
   }
-?>
