3a288f56fa66ce515d42778888cbfe0257cb74f0
YURI <yuri@gizra.com>
2014-12-11 14:52:13 +0200

    wip export node function.

--- export_data/createTable.sql
+++ export_data/createTable.sql
@@ -8,3 +8,14 @@ CREATE TABLE `_gizra_blog_post` (
   PRIMARY KEY (`nid`),
   KEY `nid` (`title`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `_gizra_users`;
+
+CREATE TABLE `_gizra_users` (
+  `uid` int(11) unsigned NOT NULL,
+  `name` varchar(64) DEFAULT NULL,
+  `password` varchar(64),
+  `mail` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`uid`),
+  KEY `uid` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- export_data/export_nodes.php
+++ export_data/export_nodes.php
@@ -1,30 +1,51 @@
 <?php
-function export_nodes ($type, $fields,  $table_name) {
-  $total =  db_result(db_query("SELECT COUNT(nid) FROM {node} n WHERE n.type = '%s' ORDER BY n.nid", $type));
-  $range = 50;
+//testing data
+export_nodes('news', array('nid', 'title', 'body', 'uid'),'_gizra_blog_post');
+
+/**
+ * @param $node_type
+ *  Type of node for export
+ * @param array $fields
+ *  Array fields for export to another table
+ * @param $result_table
+ *  Name for a new table for move data
+ * @return bool|mysqli_result|resource
+ *
+ */
+function export_nodes ($node_type, $fields = array(), $result_table) {
+  if(empty($node_type) || empty($fields)|| empty($result_table)){
+    return;
+  }
+  $total =  db_result(db_query("SELECT COUNT(nid) FROM {node} n WHERE n.type = '%s' ORDER BY n.nid", $node_type));
+  db_query("TRUNCATE TABLE  `_gizra_blog_post`"); //temporary
+  $range = 5;//temp
   $count = 0;
 
+  $total=5; //temporary
+  print_r($list_fields);
+  // TODO: string of directives appropriate to the type of data.
+  $directives = "'%d', '%s', '%s', '%d'";
+  $values = "";
+  foreach($fields as $item) {
+    $values .= "\$node->$item, ";
+  }
+  print_r($values);
+
   while($count < $total){
-    $result = db_query("SELECT nid, type FROM {node} n WHERE n.type = '%s' ORDER BY n.nid LIMIT %d OFFSET %d", $type, $range, $count);
+    $result = db_query("SELECT nid FROM {node} n WHERE n.type = '%s' ORDER BY n.nid LIMIT %d OFFSET %d", $node_type, $range, $count);
     while ($row = db_fetch_array($result)) {
       $node = node_load($row['nid']);
-      $list_fields = implode(", ", $fields);
-      $directives = "'%d', '%s', '%s', '%d'";
-      $values = "";
-      foreach($fields as $item) {
-        $values += "\$node -> $item, ";
-      }
-
-      $insert = db_query("INSERT INTO $table_name($list_fields)) VALUES($directives)",
-        $values );
+      // Prepare the query:
+      $query = "INSERT INTO $result_table(". implode(", ", $fields) .") VALUES(" . $directives . ")";
+      $insert = db_query($query, $values);
       ++$count;
+      $params = array(
+        '@count' => $count,
+        '@total' => $total,
+        '@id' => $node->nid,
+      );
+      drush_print(dt('(@count / @total) Processed node ID @id.', $params));
     }
-
-    $params = array(
-      '@count' => $count,
-      '@total' => $total,
-      '@id' => $node->nid,
-    );
-    drush_print(dt('(@count / @total) Processed node ID @id.', $params));
   }
+  return $insert;
 }
