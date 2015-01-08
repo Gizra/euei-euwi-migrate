0aa09681934a9e0cc719d6e4ecff6e74511375a3
YURI <yuri@gizra.com>
2015-01-08 19:14:30 +0200

    Fix User ID and Group ID for Documents.

--- export_data/ExportNodeBase.php
+++ export_data/ExportNodeBase.php
@@ -109,19 +109,14 @@ class ExportNodeBase extends ExportBase {
    * {@inheritdoc}
    */
   protected function getValues($entity) {
-    // First value for unique ID.
-    $values = $this->getEntityUniqueId($entity);
-    foreach($this->getFields() as $key => $directive) {
+    $values = parent::getValues($entity);
+    foreach($values as $key => $directive) {
       if ($key == 'gid') {
         $values[$key] = $this->getGroupIdFromEntity($entity);
       }
-      elseif ($key == 'uid') {
+      if ($key == 'uid') {
         $values[$key] = $this->getSiteName() . ':' . $entity->$key;
       }
-      else {
-        $values[$key] = $entity->$key;
-      }
-
     }
     return $values;
   }
