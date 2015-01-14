6ef6c4f828b0649ba090ea68d9a7018917dd63b9
YURI <yuri@gizra.com>
2015-01-14 14:58:04 +0200

    Added unprotect function for emails.

--- export_data/prepare/protect_email.php
+++ export_data/prepare/protect_email.php
@@ -3,23 +3,20 @@
 /**
  * @file
  * Protect email.
+ *
+ * For unprotect emails use: drush scr protect.php --unprotect=1
  */
 
-$id = drush_get_option('id', 1);
-$total = db_result(db_query("SELECT COUNT(uid) FROM {users} u ORDER BY u.uid", $id));
+$unprotect = drush_get_option('unprotect', 0);
+$total = db_result(db_query("SELECT COUNT(uid) FROM {users} u WHERE uid != 0 ORDER BY u.uid ", $id));
 $range = 50;
 $count = 0;
 
 while ($count < $total){
-  $result = db_query("SELECT uid FROM {users} u  ORDER BY u.uid LIMIT %d OFFSET %d", $range, $count);
+  $result = db_query("SELECT uid FROM {users} u WHERE uid != 0 ORDER BY u.uid LIMIT %d OFFSET %d ", $range, $count);
   while ($user = user_load(db_fetch_array($result))) {
 
-    $mail = $user->mail;
-    $mail = explode('.', $mail);
-    if (end($mail) != 'test') {
-      $user->mail .= '.test';
-      $insert = db_query("UPDATE users SET mail = '%s' WHERE uid = '%d'", $user->mail, $user->uid);
-    }
+    $unprotect == 1 ? unprotect($user) : protect($user);
 
     $count++;
     $params = array(
@@ -30,4 +27,25 @@ while ($count < $total){
 
     drush_print(dt('(@count / @total) Processed user ID @id.', $params));
   }
+}
+
+function protect($user) {
+  $mail = explode('.', $user->mail);
+  if (end($mail) == 'test') {
+    return;
+  }
+
+  $user->mail .= '.test';
+  db_query("UPDATE users SET mail = '%s' WHERE uid = '%d'", $user->mail, $user->uid);
+}
+
+function unprotect($user) {
+  $mail = explode('.', $user->mail);
+  if (end($mail) != 'test') {
+    return;
+  }
+
+  array_pop($mail);
+  $user->mail = implode('.', $mail);
+  db_query("UPDATE users SET mail = '%s' WHERE uid = '%d'", $user->mail, $user->uid);
 }
