1f8db6489b57642f6764201c88d24922d8b4e823
YURI <yuri@gizra.com>
2015-01-19 14:36:28 +0200

    Add user login

--- behat/features/blog_post.feature
+++ behat/features/blog_post.feature
@@ -1,15 +1,18 @@
 Feature: Blog post
-  In order to be able to view a book
+  In order to be able to view a blog post
   As an anonymous user
-  We need to be able to have access to a book page
+  We need to be able to have access to a blog post page
 
   @api
   Scenario Outline: Visit a blog post page
     Given I am an anonymous user
     When  I visit "<url>"
     Then  I should get a "200" HTTP response
-    And   I should see the text "<text>"
+    And   I should see the text "<title>"
+    And   I should see the text "<body>"
+    And I should see the text "<categories>"
+    And I should see the text "<author>"
 
   Examples:
-    | url                  | text                                  |
-    | public-water_and_sanitation/blog/bill-gates-boit-de-leau-produite-à-partir-dexcréments | Bill Gates boit de l'eau produite à partir |
+    | url                  | title                                  | body | categories | author |
+    | public-water_and_sanitation/blog/bill-gates-boit-de-leau-produite-à-partir-dexcréments | Bill Gates boit de l'eau produite à partir | http://www.franceinfo.fr/actu/monde/article/bill-gates-boit-de-l-eau-produite-partir-d-excrements-628153 | Water supply | Antoine Saintraint |
--- behat/features/bootstrap/FeatureContext.php
+++ behat/features/bootstrap/FeatureContext.php
@@ -7,6 +7,7 @@ use Behat\Behat\Context\ClosuredContextInterface,
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
 use Drupal\DrupalExtension\Context\DrupalContext;
+use Behat\Behat\Context\Step;
 
 //
 // Require 3rd-party libraries here:
@@ -18,28 +19,56 @@ use Drupal\DrupalExtension\Context\DrupalContext;
 /**
  * Features context.
  */
-class FeatureContext extends DrupalContext
-{
+class FeatureContext extends DrupalContext {
+
+  protected $drupalUsers = array();
     /**
      * Initializes context.
      * Every scenario gets its own context object.
      *
      * @param array $parameters context parameters (set them up through behat.yml)
      */
-    public function __construct(array $parameters)
-    {
-        // Initialize your context here
+    public function __construct(array $parameters) {
+      if (isset($parameters['drupal_users'])) {
+        $this->drupalUsers = $parameters['drupal_users'];
+      }
+      else {
+        throw new Exception('behat.yml should include "drupal_users" property.');
+      }
     }
 
-//
-// Place your definition and hook methods here:
-//
-//    /**
-//     * @Given /^I have done something with "([^"]*)"$/
-//     */
-//    public function iHaveDoneSomethingWith($argument)
-//    {
-//        doSomethingWith($argument);
-//    }
-//
+  /**
+   * Authenticates a user with password from configuration.
+   *
+   * @Given /^I logging in as "([^"]*)"$/
+   */
+  public function iAmLoggingInAs($username) {
+    try {
+      $password = $this->drupalUsers[$username];
+    }
+    catch (Exception $e) {
+      throw new Exception("Password not found for '$username'.");
+    }
+    if ($this->getDriver() instanceof Drupal\Driver\DrushDriver) {
+      // We are using a cli, log in with meta step.
+      return array(
+        new Step\When('I am not logged in'),
+        new Step\When('I visit "/user"'),
+        new Step\When('I fill in "Username" with "' . $username . '"'),
+        new Step\When('I fill in "Password" with "' . $password . '"'),
+        new Step\When('I press "edit-submit"'),
+      );
+    }
+    else {
+      // Log in.
+      // Go to the user page.
+      $element = $this->getSession()->getPage();
+      $this->getSession()->visit($this->locatePath('/user'));
+      $element->fillField('Username', $username);
+      $element->fillField('Password', $password);
+      $submit = $element->findButton('Log in');
+      $submit->click();
+    }
+  }
+
 }
