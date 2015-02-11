d51b875ad95721f1c5feb5e93b179f4b84ec90d8
Yuri Failer <yuri@gizra.com>
2015-02-11 11:06:22 +0200

    #155 Export attached files for news, event.
    Fixed taxonomy for all nodes.
    Create a zip from the files for Documents with >1 attachment.

--- export_data/ExportNodeBase.php
+++ export_data/ExportNodeBase.php
@@ -139,7 +139,8 @@ class ExportNodeBase extends ExportBase {
         $values[$key] = $this->getCounterFromNode($entity);
       }
       elseif ($key == 'ref_documents') {
-        $values[$key] = $this->addReferenceDocument($entity);
+        //Documents hasn't images and files exports as document.
+        $values[$key] = $this->getOriginalBundle()=='ipaper' ? "" : $this->getReferenceDocument($entity);
       }
     }
     return $values;
@@ -271,10 +272,8 @@ class ExportNodeBase extends ExportBase {
    * @throws Exception
    *   message if destination directory not exist.
    */
-  protected function exportFile($file) {
+  protected function exportFile($file, $folder='files') {
     $file = is_array($file) ? (object)$file : $file;
-    //Set a different folders for different content type.
-    $folder = $this->getOriginalBundle() == 'news' ? 'images' : 'files';
     $destination = "../euei/export_data/" . $folder . "/" . $this->getSiteName();
 
     if (!file_check_directory($destination, FILE_CREATE_DIRECTORY)) {
@@ -302,16 +301,136 @@ class ExportNodeBase extends ExportBase {
    * separated by pipe.
    *
    * A document is already migrated in ExportNodeDocumentImage.
+   * And files related to node (News, Event).
    *
    * @param $entity
    *  The entity object of type node.
    *
-   * @return bool.
+   * @return string.
+   *  The ID documents separated by pipe or false.
    */
-  protected function addReferenceDocument($entity) {
+  protected function getReferenceDocument($entity) {
+
+    $ref_documents = array();
+
     if (!empty($entity->field_image[0])) {
-      return $this->getSiteName() . ':image:' . $this->getEntityId($entity);
+      $ref_documents[] = $this->getSiteName() . ':image:' . $this->getEntityId($entity);
+    }
+
+    if (!empty($entity->files)) {
+      if(count($entity->files) > 1) {
+        $file = $this->createZip($entity);
+      }
+      else {
+        $path = $this->exportFile(reset($entity->files));
+        $file = array (
+          'file_name'=> end(explode('/', $path)),
+          'file_path'=> $path,
+        );
+      }
+
+      $ref_documents[] = $this->addFileAsDocument($file, $entity);
+
+    }
+
+    return count($ref_documents)? implode('|', $ref_documents) : FALSE;
+  }
+
+  /**
+   * Create zip archive from related node files.
+   *
+   * @param $entity
+   *   The entity object of type node.
+   *
+   * @return array
+   *   The array with  the 'filename' and 'filepath' elements for new zip file.
+   *
+   * @throws Exception
+   *   Message if zip file could not be created.
+   */
+  protected function createZip($entity) {
+    $valid_files = array();
+    foreach ($entity->files as $file) {
+      $filepath = $this->getSiteName() == 'euwi' ? file_directory_path() . '/' . $file->filepath : $file->filepath;
+      if (file_exists($filepath)) {
+        $valid_files[] = $filepath;
+      } else {
+        drush_print(dt('Reference file @source could not be found.', array('@source' => $filepath)));
+      }
+    }
+
+    if (count($valid_files)) {
+      $destination = '../euei/export_data/files/' . $this->getSiteName() . '/' . $this->getSiteName() . '_' . $this->getEntityId($entity) . '.zip';
+      $zip = new ZipArchive();
+      if($zip->open($destination, ZIPARCHIVE::OVERWRITE) !== true) {
+        throw new Exception(strstr('Cannot create zip file @dest ', array('@dest' => $destination)));
+      }
+      foreach($valid_files as $file) {
+        $name = "/" . end(explode('/', $file));
+        $zip->addFile($file, $name);
+      }
+      $zip->close();
+      if (file_exists($destination)) {
+        $destination = explode('/', $destination);
+        $destination = array_slice($destination, 2);
+        $zipfile = array(
+          'file_name' => end($destination),
+          'file_path' => implode('/', $destination),
+        );
+
+        return $zipfile;
+      }
     }
   }
+
+
+  /**
+   * @param $file
+   *  Array contains 'file_name" and "file_path" elements.
+   * @param $entity
+   *   The entity ID object.
+   *
+   * @return string
+   *   The uniqueID
+   */
+  protected function addFileAsDocument($file, $entity) {
+
+    $fields = array(
+      'unique_id' => '%s',
+      'title' => '%s',
+      'uid' => '%s',
+      'created' => '%d',
+      'file_name' => '%s',
+      'file_path' => '%s',
+    );
+
+    $directives = array();
+    foreach ($fields as $directive) {
+      $directives[] = "'" . $directive . "'";
+    }
+
+    $values = array();
+    foreach($fields as $key => $directive) {
+      if ($key == 'unique_id') {
+        $values[$key] = $this->getSiteName() . ':file:' . $this->getEntityId($entity);
+      }
+      elseif ($key == 'title') {
+        $values[$key] = $file['file_name'];
+      }
+      elseif ($key == 'uid') {
+        $values[$key] = $this->getSiteName() . ':' . $entity->uid;
+      }
+      elseif ($key == 'created') {
+        $values[$key] = $entity->created;
+      }
+      else {
+        $values[$key] = $file[$key];
+      }
+    }
+
+    $query = "INSERT INTO euei._gizra_node_document(". implode(", ", array_keys($fields)) .") VALUES(" . implode(", ", $directives) . ")";
+    return db_query($query, $values) ? $values['unique_id'] : FALSE ;
+  }
+
 }
 
